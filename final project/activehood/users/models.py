ad4eaa014a38c5b8d6759500b2c9ac9be4b2a547
Alina <alina.zaslavska@dci-student.org>
2024-07-04 12:02:35 +0200

    user

--- final project/activehood/users/models.py
+++ final project/activehood/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from .choices import ACTIVITIES_CHOICES, CITY_CHOICES
 
 class Activity(models.Model):
@@ -7,31 +8,19 @@ class Activity(models.Model):
     def __str__(self):
         return self.name
 
-class User(models.Model):
+class CustomUser(AbstractUser):
     # Personal information
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
     date_of_birth = models.DateField()
-    
-    # Authentication
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=100)  # In practice, consider using Django's built-in password hashing
-    
-    # City in Germany
     city = models.CharField(max_length=50, choices=CITY_CHOICES)
     
-    # Preferred activities (many-to-many relationship with Activity model)
-    preferred_activities = models.ManyToManyField(Activity, blank=True)
-    
-    # Profile picture
+    # Additional fields
     profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    
-    # About myself
     about_myself = models.TextField(blank=True)
     
-    # Friends (many-to-many relationship with self)
+    # Relationships
+    preferred_activities = models.ManyToManyField(Activity, blank=True)
     friends = models.ManyToManyField('self', symmetrical=True, blank=True)
-    
+
     def __str__(self):
-        return f'{self.name} {self.surname}'
+        return f'{self.username} ({self.get_full_name()})'
     
