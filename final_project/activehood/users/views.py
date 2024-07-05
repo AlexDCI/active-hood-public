c2b840e9706be8ee7e890ec03cdab6019cdacb7a
Alina <alina.zaslavska@dci-student.org>
2024-07-05 10:37:30 +0200

    new

--- final_project/activehood/users/views.py
+++ final_project/activehood/users/views.py
@@ -13,21 +13,6 @@ def home_view(request):
 
 
 def register_view(request):
-    if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(
-                request, f"Account created for {username}. You can now log in."
-            )
-            return redirect(
-                "login"
-            )  # Redirect to login page after successful registration
-    else:
-        form = CustomUserCreationForm()
-    return render(request, "users/register.html", {"form": form})
-
-
+    pass
 def login_view(request):
     pass
