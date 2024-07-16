d8808d1d4826a5e30a55f43580e764ff93ead927
Louise <louise.gauck@dci-student.org>
2024-07-16 15:14:28 +0200

    events updated, not finished

--- final_project/activehood/events/models.py
+++ final_project/activehood/events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from locations.models import City
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.ForeignKey(City, on_delete=models.CASCADE, null=True) #temporarily optional
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    #participation = models.ForeignKey(User, )
+    
+    def __str__(self):
+        return self.name
