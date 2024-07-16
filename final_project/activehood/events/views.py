1889136b9c53196deb726cc274b853080909fd5b
Louise <louise.gauck@dci-student.org>
2024-07-16 16:59:46 +0200

    deleted env

--- final_project/activehood/events/views.py
+++ final_project/activehood/events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from events.models import Event
