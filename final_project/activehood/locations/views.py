86ab9618f46a68c807eaa45634ea83880bd935f4
Alina <alina.zaslavska@dci-student.org>
2024-07-19 13:02:59 +0200

    locations

--- final_project/activehood/locations/views.py
+++ final_project/activehood/locations/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
+from locations.models import Bundesland, City
 from django.shortcuts import render
 
-# Create your views here.
+
+def load_cities(request):
+
+    bundesland_id = request.GET.get('bundesland_id')
+    print( bundesland_id)
+    cities = City.objects.filter(bundesland_id=bundesland_id).order_by('name')
+    print(cities)
+    return JsonResponse({'cities': [{'id': city.id, 'name': city.name} for city in cities]})
