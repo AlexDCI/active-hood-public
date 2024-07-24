cc38425b603bd70ed4d327d4889c9a968d5ab85a
Louise <louise.gauck@dci-student.org>
2024-07-24 13:08:22 +0200

    api views and urls updated, token auth added

--- final_project/activehood/events/urls.py
+++ final_project/activehood/events/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from . import views
+from . import api_views, views
 
 urlpatterns = [
-    path("", views.EventListByLocation.as_view(), name="events_home"), # needs update
-    path("create/", views.CreateEvent.as_view(), name='events_create'),
-    path('<int:pk>/', views.EventDetail.as_view(), name='events_detail'),
-    path('myevents/', views.MyEventsList.as_view(), name='events_myevents'),
-    path('participating/', views.MyParticipationList.as_view(), name='events_participating'), 
-    path('user/<int:user_id>/', views.UserEventsList.as_view(), name='events_userevents'),
-    path('join/<int:pk>/', views.JoinEvent.as_view(), name='events_join'),
-    path('leave/<int:pk>/', views.LeaveEvent.as_view(), name='events_leave'),
+    path("", api_views.EventListByLocation.as_view(), name="api_events_home"), 
+    path("create/", api_views.CreateEvent.as_view(), name='api_events_create'),
+    path('<int:pk>/', api_views.EventDetail.as_view(), name='api_events_detail'),
+    path('myevents/', api_views.MyEventsList.as_view(), name='api_events_myevents'),
+    path('participating/', api_views.MyParticipationList.as_view(), name='api_events_participating'), 
+    path('user/<int:user_id>/', api_views.UserEventsList.as_view(), name='api_events_userevents'),
+    path('join/<int:pk>/', api_views.JoinEvent.as_view(), name='api_events_join'),
+    path('leave/<int:pk>/', api_views.LeaveEvent.as_view(), name='api_events_leave'),
+    # regular views
+    #path('display/', views.display_event, name='display'),
 ]
