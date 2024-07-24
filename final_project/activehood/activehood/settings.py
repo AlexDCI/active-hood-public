0de539d10558951d7a6b90a903d4498ec8f85a27
Louise <louise.gauck@dci-student.org>
2024-07-24 13:14:16 +0200

    added authentication settings

--- final_project/activehood/activehood/settings.py
+++ final_project/activehood/activehood/settings.py
@@ -196,3 +196,11 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
